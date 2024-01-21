1baf0ff6bc314c34f300ecf51fbe31760ddaf223
Prajwol Man Shakya <shakya.prajwol@outlook.com>
2024-01-21 19:14:18 +0545

    Catching Errors with TypeScript

--- Typescript/fetchjson/index.js
+++ Typescript/fetchjson/index.js
@@ -3,5 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var axios_1 = require("axios");
 var url = 'https://jsonplaceholder.typicode.com/todos/1';
 axios_1.default.get(url).then(function (response) {
-    console.log(response.data);
+    var todo = response.data;
+    var id = todo.id;
+    var title = todo.title;
+    var completed = todo.completed;
+    console.log("\n        The Todo with id: ".concat(id, "\n        Has a title of: ").concat(title, "\n        Is it completed?: ").concat(completed, "\n    "));
 });
