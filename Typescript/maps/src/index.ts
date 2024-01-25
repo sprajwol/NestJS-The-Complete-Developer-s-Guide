11590072c38d8137f66e649fa344bf0e2d2f79a5
Prajwol Man Shakya <shakya.prajwol@outlook.com>
2024-01-25 19:06:28 +0545

    Exploring Type Definition Files

--- Typescript/maps/src/index.ts
+++ Typescript/maps/src/index.ts
@@ -1,10 +1,12 @@
-import { User } from './User';
-import { Company } from './Company';
+/// <reference types="@types/google.maps" />
 
-const user = new User();
+// import { User } from './User';
+// import { Company } from './Company';
 
-console.log(user);
-
-const company = new Company();
-
-console.log(company);
+new google.maps.Map(document.getElementById('map') as HTMLElement, {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0,
+    },
+});
