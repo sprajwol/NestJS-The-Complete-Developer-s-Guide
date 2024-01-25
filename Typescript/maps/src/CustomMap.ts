d050e775775b145261eacb08b6759a09db6fecb8
Prajwol Man Shakya <shakya.prajwol@outlook.com>
2024-01-25 19:22:37 +0545

    Why Use Private Modifiers? Here's Why

---
+++ Typescript/maps/src/CustomMap.ts
@@ -0,0 +1,18 @@
+/// <reference types="@types/google.maps" />
+
+export class CustomMap {
+    private googleMap: google.maps.Map;
+
+    constructor(divId: string) {
+        this.googleMap = new google.maps.Map(
+            document.getElementById(divId) as HTMLElement,
+            {
+                zoom: 1,
+                center: {
+                    lat: 0,
+                    lng: 0,
+                },
+            }
+        );
+    }
+}
--- Typescript/maps/src/index.ts
+++ Typescript/maps/src/index.ts
@@ -1,12 +1,5 @@
-/// <reference types="@types/google.maps" />
-
 // import { User } from './User';
 // import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
-new google.maps.Map(document.getElementById('map') as HTMLElement, {
-    zoom: 1,
-    center: {
-        lat: 0,
-        lng: 0,
-    },
-});
+new CustomMap('map');
