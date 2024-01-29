7d057f73ae79116e4b70764af85b5e19b1686262
Prajwol Man Shakya <shakya.prajwol@outlook.com>
2024-01-29 18:50:59 +0545

    How to Build Interceptors

--- NestJS/mycv/src/users/users.controller.ts
+++ NestJS/mycv/src/users/users.controller.ts
@@ -8,10 +8,12 @@ import {
   Delete,
   Patch,
   NotFoundException,
+  UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
+import { SerializeInterceptor } from '../interceptors/serialize.interceptor';
 
 @Controller('auth')
 export class UsersController {
@@ -22,8 +24,10 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
+  @UseInterceptors(SerializeInterceptor)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
+    console.log('handler is running');
     const user = await this.usersService.findOne(parseInt(id));
     if (!user) {
       throw new NotFoundException('user not found');
