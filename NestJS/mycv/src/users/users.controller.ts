6f811d868a65eb0e18e227288327eed79ae4bd96
Prajwol Man Shakya <shakya.prajwol@outlook.com>
2024-01-30 18:11:11 +0545

    Small Fix for CurrentUserInterceptor

--- NestJS/mycv/src/users/users.controller.ts
+++ NestJS/mycv/src/users/users.controller.ts
@@ -16,6 +16,7 @@ import { UsersService } from './users.service';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -25,9 +26,19 @@ export class UsersController {
     private authService: AuthService,
   ) {}
 
+  // @Get('whoami')
+  // async whoAmI(@Session() session: any) {
+  //   return this.usersService.findOne(session.userId);
+  // }
+
   @Get('whoami')
-  async whoAmI(@Session() session: any) {
-    return this.usersService.findOne(session.userId);
+  async whoAmI(@CurrentUser() user: any) {
+    return user;
+  }
+
+  @Post('signout')
+  signOut(@Session() session: any) {
+    session.userId = null;
   }
 
   @Post('signup')
