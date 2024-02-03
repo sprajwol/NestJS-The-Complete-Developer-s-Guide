5fe95baf2d11601017706e68cf441bcf378e5ce0
Prajwol Man Shakya <shakya.prajwol@outlook.com>
2024-02-03 22:12:09 +0545

    Refactor 'Unused Email' Test to Address Jest Breaking Changes

--- NestJS/mycv/src/users/auth.service.spec.ts
+++ NestJS/mycv/src/users/auth.service.spec.ts
@@ -2,7 +2,7 @@ import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
-import { BadRequestException } from '@nestjs/common';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let service: AuthService;
@@ -44,5 +44,11 @@ describe('AuthService', () => {
     await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
       BadRequestException,
     );
+  });
+
+  it('throws if signin is called with an unused email', async () => {
+    await expect(
+      service.signin('asdflkj@asdlfkj.com', 'passdflkj'),
+    ).rejects.toThrow(NotFoundException);
   });
 });
